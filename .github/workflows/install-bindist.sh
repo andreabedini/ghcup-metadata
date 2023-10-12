c73b803655e020343a1de3f0435b48b12a5d521d
Ben Gamari <ben@smart-cactus.org>
2023-10-12 12:44:53 -0400

    blah

--- .github/workflows/install-bindist.sh
+++ .github/workflows/install-bindist.sh
@@ -21,98 +21,13 @@ case $TOOL in
 	ghcup)
 		ghcup_fun upgrade --force
 		;;
-	*) ghcup_fun install "$TOOL" --set "$VERSION"
-		;;
-esac
-
-mkdir -p /tmp/install-bindist-ci
-cp "$METADATA_FILE" /tmp/install-bindist-ci/
-cd /tmp/install-bindist-ci
-
-cat <<EOF > main.hs
-{- cabal:
-build-depends: base
--}
-
-main = print $ 1 + 1
-EOF
-
-case $TOOL in
-	ghcup)
-		ghcup_fun list
-		;;
-	hls)
-		ghcup_fun install cabal latest
-		ghcup_fun install ghc --set recommended
-		cabal update
-
-		test_package="bytestring-0.11.1.0"
-		test_module="Data/ByteString.hs"
-
-		create_cradle() {
-			echo "cradle:" > hie.yaml
-			echo "  cabal:" >> hie.yaml
-		}
-
-		enter_test_package() {
-			local tmp_dir
-			tmp_dir=$(mktempdir)
-			cd "$tmp_dir"
-			cabal unpack "${test_package}"
-			cd "${test_package}"
-			cp "/tmp/install-bindist-ci/${METADATA_FILE}" .
-		}
-
-		# For all HLS GHC versions and the wrapper, run 'typecheck'
-		# over the $test_module
-		test_all_hls() {
-			local bin
-			local bin_noexe
-			local bindir
-			local hls
-			bindir=$1
-
-			for hls in "${bindir}/"haskell-language-server-* ; do
-				bin=${hls##*/}
-				bin_noexe=${bin/.exe/}
-				if ! [[ "${bin_noexe}" =~ "haskell-language-server-wrapper" ]] && ! [[ "${bin_noexe}" =~ "~" ]] && ! [[ "${bin_noexe}" =~ ".shim" ]] ; then
-					if ghcup_fun install ghc --set "${bin_noexe/haskell-language-server-/}" ; then
-						"${hls}" typecheck "${test_module}" || fail "failed to typecheck with HLS for GHC ${bin_noexe/haskell-language-server-/}"
-					else
-						fail "GHCup failed to install GHC ${bin_noexe/haskell-language-server-/}"
-					fi
-					ghcup_fun rm ghc "${bin_noexe/haskell-language-server-/}"
-				fi
-			done
-			"$bindir/haskell-language-server-wrapper${ext}" typecheck "${test_module}" || fail "failed to typecheck with HLS wrapper"
-		}
-
-        enter_test_package
-        create_cradle
-		case "$(uname -s)" in
-			MSYS_*|MINGW*)
-				test_all_hls "$(dirname "$(which ghcup)")"
-				;;
-			*)
-				test_all_hls "$(ghcup whereis bindir)"
-			   ;;
-	    esac
-		;;
-    ghc)
-		ghc --version
-		ghc --info
-		ghc -prof main.hs
-		[[ $(./main +RTS -s) -eq 2 ]]
-		;;
     cabal)
-		ghcup_fun install ghc --set "$(ghcup_fun list -t ghc -r -c available | tail -1 | awk '{ print $2 }')"
-		cabal --version
-		cabal update
-		ghc -prof main.hs -o hi && ./hi
-        cp -R hi* C:/cabal
-        [[ $(cabal --store-dir=$(pwd) --verbose=2 --ghc-options=-v3 run --enable-profiling ./main.hs -- +RTS -s) -eq 2 ]]
-		;;
-    *)
-		$TOOL --version
+        ghcup_fun install "$TOOL" --set "$VERSION"
+        ghc_ver="$(ghcup_fun list -t ghc -r -c available | tail -1 | awk '{ print $2 }')"
+		ghcup_fun install ghc --set "$ghc_ver"
+        ;;
+	*)
+        ghcup_fun install "$TOOL" --set "$VERSION"
 		;;
 esac
+
